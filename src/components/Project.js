1cbc8777e624ba4a746ab9c53b1aacd715bbddcb
Jacob Colborn <jake@jakesweb.me>
2019-10-04 14:08:20 -0400

    added projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,1 +1,19 @@
-rcc
+import React, { Component } from "react"
+import styled from "styled-components"
+
+class Project extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <img href={this.props.link} alt={this.props.title} />
+        <p>{this.props.title}</p>
+      </div>
+    )
+  }
+}
+
+export default Project
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,16 @@ import React from "react"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
+import Project from "../components/Project"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <Project
+      link="https://res.cloudinary.com/jakes-web/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1560478939/sickfits/ed4ribdtaavisodktany.png"
+      title="river,bike"
+    />
   </Layout>
 )
 

8bc0513b063e29ca9350dfc559e5f4cc8ba8cd8e
Jacob Colborn <jake@jakesweb.me>
2019-12-08 16:59:54 -0500

    image changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import Project from "../components/Project"
 import Contact from "../components/Contact"
 import SEO from "../components/seo"

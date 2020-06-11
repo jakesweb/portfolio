22c8f0bcad9c15c7d4dc77fa6785988f6cf29e3f
Jacob Colborn <jake@jakesweb.me>
2020-06-10 23:25:02 -0400

    correcting netlify form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,12 @@
-import React from "react"
+import React, { Component } from "react"
 import styled from "styled-components"
+import { navigateTo } from "gatsby"
+
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
+}
 
 const HeaderText = styled.h1`
   text-align: center;
@@ -26,30 +33,75 @@ const StyledForm = styled.form`
   }
 `
 
-const Contact = () => (
-  <div>
-    <HeaderText>
-      Feel free to contact me with any questions or opprotunities. Thank you!
-    </HeaderText>
-    <StyledForm name="contact" data-netlify="true">
-      <label>
-        Name
-        <input type="text" name="name" />
-      </label>
-      <label>
-        Email
-        <input type="email" name="email" />
-      </label>
-      <label>
-        Message
-        <textarea name="message" cols="30" rows="10" />
-      </label>
-      <br />
-      <button type="submit" name="submit" id="submit">
-        Contact
-      </button>
-    </StyledForm>
-  </div>
-)
+class Contact extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
 
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleSubmit = e => {
+    e.preventdefault()
+    const form = e.target
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...this.state,
+      }),
+    })
+      .then(() => navigateTo(form.getAttribute("action")))
+      .catch(error => alert(error))
+  }
+  render() {
+    return (
+      <div>
+        <HeaderText>
+          Feel free to contact me with any questions or opprotunities. Thank
+          you!
+        </HeaderText>
+        <StyledForm
+          name="contact"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          action="/thanks"
+          method="post"
+          onSubmit={this.handleSubmit}
+        >
+          <p hidden>
+            <label>
+              Dont' Fill this out
+              <input name="bot-field" onChange={this.handleChange} />
+            </label>
+          </p>
+          <label>
+            Name
+            <input type="text" name="name" onChange={this.handleChange} />
+          </label>
+          <label>
+            Email
+            <input type="email" name="email" onChange={this.handleChange} />
+          </label>
+          <label>
+            Message
+            <textarea
+              name="message"
+              cols="30"
+              rows="10"
+              onChange={this.handleChange}
+            />
+          </label>
+          <br />
+          <button type="submit" name="submit" id="submit">
+            Contact
+          </button>
+        </StyledForm>
+      </div>
+    )
+  }
+}
 export default Contact
---
+++ src/pages/thanks.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ThanksPage = () => (
+  <Layout>
+    <SEO title="Thanks" />
+    <h1>Thank you for the submission!</h1>
+  </Layout>
+)
+
+export default ThanksPage
